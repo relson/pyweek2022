0d76ad81e566ed6a6dbac3184e25548fcd4383fe
Sebastião Relson <relsonto@gmail.com>
2022-08-14 14:11:44 +0000

    day3 test unitário

---
+++ Makefile
@@ -0,0 +1,7 @@
+test, testar: init
+	python3 -m pytest -v
+
+
+init:
+	figlet "BEERLOG"
+	figlet "PyWeek2022"
---
+++ tests/test_core.py
@@ -0,0 +1,12 @@
+from beerlog.core import get_beers_from_database, add_beer_to_database
+
+def test_add_beer_to_database():
+    assert add_beer_to_database("Blue Moon", "Witebier", 10, 3, 6)
+
+def test_get_beers_from_database():
+    # Arrange
+    add_beer_to_database("Blue Moon", "Witebier", 10, 3, 6)
+    # Act
+    results = get_beers_from_database()
+    # Assert
+    assert len(results) > 0
